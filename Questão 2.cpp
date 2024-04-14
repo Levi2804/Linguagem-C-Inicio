0188b43f7acc8d282ef1708ef2f34a5eb32ed796
Levi2804 <166538311+Levi2804@users.noreply.github.com>
2024-04-14 19:33:43 -0300

    Add files via upload

---
+++ Questão 2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	printf("Bom, dia!\n\n");
+	printf("Que,\n");
+	printf("legal!\n\n");
+	printf("\tMuito, Bom!\n");
+	printf("Bem,\n");
+	printf("\tVindo!");
+	return(0);
+}
