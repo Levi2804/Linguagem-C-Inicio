5694ba087f0a0bb2f55597722a2ff758a6550a5b
Levi2804 <166538311+Levi2804@users.noreply.github.com>
2024-04-14 19:31:32 -0300

    Add files via upload

---
+++ Questão 1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	printf("Hello, World!\n\n");
+	printf("Hello,\n");
+	printf("World!\n\n");
+	printf("\tHello, World!\n");
+	printf("Hello,\n");
+	printf("\tWorld!");
+	return(0);
+}
