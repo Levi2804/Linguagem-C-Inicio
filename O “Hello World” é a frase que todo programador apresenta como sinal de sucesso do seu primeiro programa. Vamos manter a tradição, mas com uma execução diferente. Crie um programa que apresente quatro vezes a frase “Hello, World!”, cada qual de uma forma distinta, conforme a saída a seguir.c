939930d3c9d55772997c85567b04525262fbaf20
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 14:01:45 -0300

    Update and rename Questão 1.c to O “Hello World” é a frase que todo programador apresenta como sinal de sucesso do seu primeiro programa. Vamos manter a tradição, mas com uma execução diferente. Crie um programa que apresente quatro vezes a frase “Hello, World!”, cada qual de uma forma distinta, conforme a saída a seguir.c

---
+++ O “Hello World” é a frase que todo programador apresenta como sinal de sucesso do seu primeiro programa. Vamos manter a tradição, mas com uma execução diferente. Crie um programa que apresente quatro vezes a frase “Hello, World!”, cada qual de uma forma distinta, conforme a saída a seguir.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+O “Hello World” é a frase que todo programador apresenta como sinal de sucesso
+do seu primeiro programa. Vamos manter a tradição, mas com uma execução
+diferente. Crie um programa que apresente quatro vezes a frase “Hello, World!”,
+cada qual de uma forma distinta, conforme a saída a seguir.
+*/
+int main(){
+	printf("Hello, World!\n\n");
+	printf("Hello,\n");
+	printf("World!\n\n");
+	printf("\tHello, World!\n");
+	printf("Hello,\n");
+	printf("\tWorld!");
+	return(0);
+}
