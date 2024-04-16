37e1d12b520d0675dd48f23f4c6def7b96f509bb
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 14:19:09 -0300

    Update Questão 6.c

--- Questão 6.c
+++ Questão 6.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<locale.h>
-
+/*Faça um programa que solicita e lê temperatura em Fahrenheit, faz a conversão para Celsius e exibe tal resultado. 
+Para isto, utilize a seguinte fórmula: 𝐶 = (𝐹 − 32)/1,8.
+*/
 int main(){
 	setlocale(LC_ALL,"Portuguese");
 	float f, c;
