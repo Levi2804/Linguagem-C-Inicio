c68b3dc93b3d888e8cbea85efc4cd7b7ca6e7180
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 14:10:33 -0300

    Update and rename Questão 3.c to Todos os números naturais possuem antecessor, que é o número que antecede (anterior) um valor específico, e sucessor, que é o número que sucede (posterior) um valor específico. Sabendo disso, implemente um programa que solicita ao usuário a entrada de um número inteiro e apresenta seu antecessor e sucessor.c

--- Todos os números naturais possuem antecessor, que é o número que antecede (anterior) um valor específico, e sucessor, que é o número que sucede (posterior) um valor específico. Sabendo disso, implemente um programa que solicita ao usuário a entrada de um número inteiro e apresenta seu antecessor e sucessor.c
+++ Todos os números naturais possuem antecessor, que é o número que antecede (anterior) um valor específico, e sucessor, que é o número que sucede (posterior) um valor específico. Sabendo disso, implemente um programa que solicita ao usuário a entrada de um número inteiro e apresenta seu antecessor e sucessor.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include<locale.h>
-
+/*
+. Todos os números naturais possuem antecessor, que é o número que antecede
+(anterior) um valor específico, e sucessor, que é o número que sucede (posterior) um
+valor específico. Sabendo disso, implemente um programa que solicita ao usuário a
+entrada de um número inteiro e apresenta seu antecessor e sucessor. 
+*/
 int main(){
 	setlocale(LC_ALL,"Portuguese");
 	int n;
