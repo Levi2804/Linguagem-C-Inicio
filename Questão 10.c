721cbe0498f38f80569001eebad34818ad13c52b
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 14:33:57 -0300

    Update Questão 10.c

--- Questão 10.c
+++ Questão 10.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 #include<locale.h>
 #include<math.h>
-
+/*
+Você foi procurado por uma nutricionista para automatizar o cálculo do Índice de
+Massa Corpórea (IMC) de seus pacientes. Para isto, crie um programa que solicita e
+lê o peso e a altura de uma pessoa e apresenta seu IMC com duas casas decimais
+após a vírgula. O IMC é calculado por meio da seguinte fórmula:
+𝐼𝑀𝐶 = 𝑝𝑒𝑠𝑜/𝑎𝑙𝑡𝑢𝑟𝑎².
+*/
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float p, a, imc;
