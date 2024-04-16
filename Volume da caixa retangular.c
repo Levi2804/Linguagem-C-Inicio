2d709e50697d4944d26dad9f70e4bb6efaf7f21a
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 14:41:50 -0300

    Update and rename Questão 12.c to Volume da caixa retangular.c

--- Volume da caixa retangular.c
+++ Volume da caixa retangular.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<locale.h>
-
+/*
+Faça um programa para calcular o volume de uma caixa retangular. Para isto, utilize
+a seguinte fórmula:
+𝑣𝑜𝑙𝑢𝑚𝑒 = 𝑐𝑜𝑚𝑝𝑟𝑖𝑚𝑒𝑛𝑡𝑜 × 𝑙𝑎𝑟𝑔𝑢𝑟𝑎 × 𝑎𝑙𝑡𝑢𝑟𝑎
+Os valores do comprimento, da largura e da altura devem ser fornecidos pelo
+usuário.
+*/
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float v, c, l, a;
